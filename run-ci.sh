af486901c7cbc66a68ec7a491d7fe06b4bf10468
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-28 06:12:35 -0400

    hopefully fixed the whitespace thing

--- run-ci.sh
+++ run-ci.sh
@@ -3,11 +3,13 @@
 # Exit immediately if a command fails
 set -e
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt-add-repository "deb http://apt.llvm.org/$(lsb_release -sc)/ llvm-toolchain-$(lsb_release -sc)-19 main"
-sudo apt-get update
-
-sudo apt-get install -y clang-19 clang-format-19 clang-tidy-19 cmake jq
+# Enumerate dependencies
+echo "The following dependencies are required for this scipt to succeed..."
+echo "clang-19"
+echo "clang-format-19"
+echo "clang-tidy-19"
+echo "cmake"
+echo "jq"
 
 echo "Removing build directory..."
 rm -rf build
@@ -25,4 +27,3 @@ echo "Building project..."
 cmake --build build
 
 echo "CI script completed successfully."
-
