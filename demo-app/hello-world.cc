cadfb506b3fcdc32808d0a1b2115d8180a856538
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-28 07:12:02 -0400

    Bad PR on purpose to test my CI script.

--- demo-app/hello-world.cc
+++ demo-app/hello-world.cc
@@ -1,8 +1,12 @@
 #include <iostream>
 
-auto DoTheThing() -> int { return 1; }
+auto DoTheThing() -> int 
+{
+  return 1; 
+}
 
 auto main() -> int {
+  int unusedVar;
   std::cout << "Hello, world!\n";
   return 0;
 }
