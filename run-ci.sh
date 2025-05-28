15c98f6897eb860e80b9d4e122ccc62ad7383e05
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-28 09:00:38 -0400

    Use a different build folder for ci scripts.

--- run-ci.sh
+++ run-ci.sh
@@ -4,20 +4,20 @@
 set -e
 
 echo "Removing build directory..."
-rm -rf build
+rm -rf build-ci
 
 echo "Running cmake..."
-cmake -S . -B build -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19
+cmake -S . -B build-ci -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19
 
-filtered_files=$(jq -r '[.[] | select(.file | test("third-party/") | not)] | .[].file' build/compile_commands.json)
+filtered_files=$(jq -r '[.[] | select(.file | test("third-party/") | not)] | .[].file' build-ci/compile_commands.json)
 
 echo "Running clang-format..."
 echo $filtered_files | xargs clang-format-19 -i -style="Google"
 
 echo "Running clang-tidy..."
-echo $filtered_files | xargs clang-tidy-19 -warnings-as-errors="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -checks="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -p build
+echo $filtered_files | xargs clang-tidy-19 -warnings-as-errors="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -checks="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -p build-ci
 
 echo "Building project..."
-cmake --build build
+cmake --build build-ci
 
 echo "CI script completed successfully."
