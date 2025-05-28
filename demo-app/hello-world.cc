f853c68890bf85ec6dbde216ee252bbe2b001280
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-28 06:44:12 -0400

    Updated ci script so clang-tidy and clang-format don't consider third-party stuff.

--- demo-app/hello-world.cc
+++ demo-app/hello-world.cc
@@ -3,6 +3,7 @@
 auto DoTheThing() -> int { return 1; }
 
 auto main() -> int {
+  int unusedVar;
   std::cout << "Hello, world!\n";
   return 0;
 }
--- run-ci.sh
+++ run-ci.sh
@@ -3,25 +3,19 @@
 # Exit immediately if a command fails
 set -e
 
-# Enumerate dependencies
-echo "The following dependencies are required for this scipt to succeed..."
-echo "clang-19"
-echo "clang-format-19"
-echo "clang-tidy-19"
-echo "cmake"
-echo "jq"
-
 echo "Removing build directory..."
 rm -rf build
 
 echo "Running cmake..."
 cmake -S . -B build -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19
 
+filtered_files=$(jq -r '[.[] | select(.file | test("third-party/") | not)] | .[].file' build/compile_commands.json)
+
 echo "Running clang-format..."
-jq -r '.[].file' build/compile_commands.json | xargs clang-format-19 -i -style="Google"
+echo $filtered_files | xargs clang-format-19 -i -style="Google"
 
 echo "Running clang-tidy..."
-run-clang-tidy-19 -warnings-as-errors="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -checks="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -p build
+echo $filtered_files | xargs clang-tidy-19 -warnings-as-errors="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -checks="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" -p build
 
 echo "Building project..."
 cmake --build build
