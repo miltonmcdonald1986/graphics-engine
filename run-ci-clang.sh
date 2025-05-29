e4e11362e9be1d798fd3c26cb1eb68c558fd863e
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-29 16:41:49 -0400

    Enhancements to build system; can now generate code coverage report

--- run-ci-clang.sh
+++ run-ci-clang.sh
@@ -4,18 +4,18 @@
 set -e
 
 echo "Removing build directory..."
-rm -rf build-ci
+rm -rf build-ci-clang
 
 echo "Running cmake..."
-cmake -S . -B build-ci -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19
+cmake -S . -B build-ci-clang -DCMAKE_C_COMPILER=clang-19 -DCMAKE_CXX_COMPILER=clang++-19
 
-# filtered_files=$(jq -r '[.[] | select(.file | test("third-party/") | not)] | .[].file' build-ci/compile_commands.json)
+# filtered_files=$(jq -r '[.[] | select(.file | test("third-party/") | not)] | .[].file' build-ci-clang/compile_commands.json)
 filtered_files=$(find engine-lib/include/graphics-engine -name "*.h") 
 filtered_files+=" $(find engine-lib/src -name "*.h" -o -name "*.cc")"
 filtered_files+=" $(find demo-app/src -name "*.h" -o -name "*.cc")"
 
 echo "Running cppcheck..."
-echo "$filtered_files" | xargs cppcheck --force --language=c++ --std=c++23 --enable=style --check-level=exhaustive -i build -i build-ci -i tests -i third-party --inconclusive --error-exitcode=1 .
+echo "$filtered_files" | xargs cppcheck --force --language=c++ --std=c++23 --enable=style --check-level=exhaustive -i build -i build-ci-clang -i build-ci-gcc -i build-ci-windows -i tests -i third-party --inconclusive --error-exitcode=1 .
 
 echo "Running clang-format..."
 echo $filtered_files | xargs clang-format-19 -i -style="Google"
@@ -25,9 +25,9 @@ echo $filtered_files | xargs clang-tidy-19 \
   --warnings-as-errors="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" \
   --checks="bugprone-*,cert-*,clang-analyzer-*,cppcoreguidelines-*,modernize-*,performance-*,readability*-" \
   --header-filter='engine-lib/include/graphics-engine/.*|engine-lib/src/.*|demo-app/src/.*' \
-  -p build-ci
+  -p build-ci-clang
 
 echo "Building project..."
-cmake --build build-ci
+cmake --build build-ci-clang
 
 echo "CI script completed successfully."
---
+++ run-ci-gcc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Exit immediately if a command fails
+set -e
+
+echo "Removing build directory..."
+rm -rf build-ci-gcc
+
+echo "Running CMake with GCC..."
+cmake -S . -B build-ci-gcc -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DENABLE_COVERAGE_GCC=ON
+
+echo "Building project..."
+cmake --build build-ci-gcc
+
+echo "Running tests..."
+ctest --test-dir build-ci-gcc/engine-tests --output-on-failure
+
+echo "Generating coverage data..."
+lcov --capture --directory build-ci-gcc --output-file build-ci-gcc/coverage.info
+lcov --remove build-ci-gcc/coverage.info "*/engine-tests/src/*" "*/third-party/googletest/*" "/usr/*" --output-file build-ci-gcc/coverage.info
+
+echo "Generating HTML coverage report..."
+genhtml build-ci-gcc/coverage.info --output-directory build-ci-gcc/coverage-html
+
+echo "CI script completed successfully."
