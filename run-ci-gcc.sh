743c03314f0e7ff96659d43d8778e3797f965518
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-05-29 22:21:46 -0400

    Update run-ci-gcc.sh

--- run-ci-gcc.sh
+++ run-ci-gcc.sh
@@ -17,8 +17,7 @@ ctest --test-dir build-ci-gcc/engine-tests --output-on-failure
 
 echo "Generating coverage data..."
 lcov --capture --directory build-ci-gcc/engine-lib --output-file build-ci-gcc/coverage.info
-# echo "Removing unwanted data..."
-# lcov --remove build-ci-gcc/coverage.info "*/engine-tests/src/*" "*/third-party/googletest/*" "/usr/*" --output-file build-ci-gcc/coverage.info
+
 echo "Generating HTML coverage report..."
 genhtml build-ci-gcc/coverage.info --output-directory build-ci-gcc/coverage-html
 
