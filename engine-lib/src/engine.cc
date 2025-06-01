75ecd0f760a997b1d010f364d406e028521bb053
miltonmcdonald1986 <miltonmcdonald1986@gmail.com>
2025-06-01 07:11:05 -0400

    Add glad lib, and make a unit test to show that it fails without a context.

--- engine-lib/src/engine.cc
+++ engine-lib/src/engine.cc
@@ -4,6 +4,8 @@
 
 #include "graphics-engine/engine.h"
 
+#include "glad/glad.h"
+
 #include "error.h"
 
 using ::graphics_engine::error::ErrorCode;
@@ -11,6 +13,12 @@ using ::graphics_engine::types::Expected;
 
 namespace graphics_engine::engine {
 
-DLLEXPORT [[nodiscard]] auto InitializeEngine() -> Expected<void> { return {}; }
+DLLEXPORT [[nodiscard]] auto InitializeEngine() -> Expected<void> 
+{
+  if (gladLoadGL() == 0)
+    return std::unexpected(make_error_code(ErrorCode::kEngineInitializationFailed));
+
+  return {};
+}
 
 }  // namespace graphics_engine::engine
